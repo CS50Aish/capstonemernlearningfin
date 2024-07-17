fed14fb30d454b1f02392628c6e2e1df97ec2050
Ubuntu <labuser@ubuntu2204.tf3myd2zt4reho0jasygqzyiwa.rx.internal.cloudapp.net>
2024-07-17 10:06:42 +0000

    Updated Capstone Full Stack Project Files

---
+++ learning-ease-pro/src/components/BookingList.js
@@ -0,0 +1,16 @@
+import React, { useEffect, useState } from "react";
+import axios from 'axios';
+
+const BookingList = () => {
+
+    const [bookings, setBookings] = useState([]);
+
+    useEffect(() => {
+        const fetchBookings = async () => {
+            try{
+                const response = await axios.
+            }
+        }
+    })
+
+}
